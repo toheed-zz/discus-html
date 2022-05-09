da1b8d536ab3cf1f524dec0e9ef9b8d3ecd13d08
Toheed Ali <toheed2012@hotmail.com>
2022-05-09 21:09:46 +0500

    map added

--- public/map-config.js
+++ public/map-config.js
@@ -1,7 +1,6 @@
 var usjsconfig = {
   "usjs1":{
-    "hover": "ALABAMA",//info of the popup
-    "url": "#",//link to any webpage
+    "hover": "ALABAMA",
     "target": "same_window",// use "new_window", "same_window", "modal", or "none"
     "upColor": "#f9d600",//default color
     "overColor": "#161515",//highlight color
@@ -9,302 +8,200 @@ var usjsconfig = {
     "active": true//true/false to activate/deactivate
   },
   "usjs2":{
-    "hover": "<b><u>ALASKA</u></b><br>Write any text and load images<br><img src='example.png'>",
-    "url": "#", "target": "same_window",
-    "upColor": "#f9d600", "overColor": "#161515", "downColor": "#161515",
+   "upColor": "#f9d600", "overColor": "#161515", "downColor": "#161515",
     "active": true
   },
   "usjs3":{
-    "hover": "<b><u>ARIZONA</u></b><br><span style='color: #bcbcbc;'>Street Address:</span><br>&nbsp;321 Example, Address 54321<br><span style='color: #bcbcbc;'>Telephone:</span><br>&nbsp;(256) 555-4321 / (256) 555-1234",
-    "url": "#", "target": "same_window",
     "upColor": "#f9d600", "overColor": "#161515", "downColor": "#161515",
     "active": true
   },
   "usjs4":{
-    "hover": "<b><u>ARKANSAS</u></b><br><span style='color: #999;'>*Click to open a webpage*</span>",
-    "url": "#", "target": "same_window",
     "upColor": "#f9d600", "overColor": "#161515", "downColor": "#161515",
     "active": true
   },
   "usjs5":{
-    "hover": "<b><u>CALIFORNIA</u></b><br><span style='color: #999;'>Click to open a modal window!</span><br><span style='color: #ff6666;'><b>Modal Window Option is Compatible<br> with Bootstrap Only.</b></span>",
-    "url": "#mymodal", "target": "modal",
     "upColor": "#f9d600", "overColor": "#161515", "downColor": "#161515",
     "active": true
   },
   "usjs6":{
-    "hover": "COLORADO",
-    "url": "#", "target": "same_window",
     "upColor": "#f9d600", "overColor": "#161515", "downColor": "#161515",
     "active": false
   },
   "usjs7":{
-    "hover": "CONNECTICUT",
-    "url": "#", "target": "same_window",
     "upColor": "#f9d600", "overColor": "#161515", "downColor": "#161515",
     "active": true
   },
   "usjs8":{
-    "hover": "DELAWARE",
-    "url": "#", "target": "same_window",
     "upColor": "#f9d600", "overColor": "#161515", "downColor": "#161515",
     "active": true
   },
   "usjs9":{
-    "hover": "FLORIDA",
-    "url": "#", "target": "same_window",
     "upColor": "#f9d600", "overColor": "#161515", "downColor": "#161515",
     "active": true
   },
   "usjs10":{
-    "hover": "GEORGIA",
-    "url": "#", "target": "same_window",
     "upColor": "#f9d600", "overColor": "#161515", "downColor": "#161515",
     "active": true
   },
   "usjs11":{
-    "hover": "HAWAII",
-    "url": "#", "target": "same_window",
     "upColor": "#f9d600", "overColor": "#161515", "downColor": "#161515",
     "active": true
   },
   "usjs12":{
-    "hover": "IDAHO",
-    "url": "#", "target": "same_window",
     "upColor": "#f9d600", "overColor": "#161515", "downColor": "#161515",
     "active": true
   },
   "usjs13":{
-    "hover": "<b><u>ILLINOIS</u></b><br>Write any text and load images<br><img src='example.png'>",
-    "url": "#", "target": "same_window",
     "upColor": "#f9d600", "overColor": "#161515", "downColor": "#161515",
     "active": true
   },
   "usjs14":{
-    "hover": "INDIANA",
-    "url": "#", "target": "same_window",
     "upColor": "#f9d600", "overColor": "#161515", "downColor": "#161515",
     "active": true
   },
   "usjs15":{
-    "hover": "IOWA",
-    "url": "#", "target": "same_window",
     "upColor": "#f9d600", "overColor": "#161515", "downColor": "#161515",
     "active": true
   },
   "usjs16":{
-    "hover": "KANSAS",
-    "url": "#", "target": "same_window",
     "upColor": "#f9d600", "overColor": "#161515", "downColor": "#161515",
     "active": true
   },
   "usjs17":{
-    "hover": "KENTUCKY",
-    "url": "#", "target": "same_window",
     "upColor": "#f9d600", "overColor": "#161515", "downColor": "#161515",
     "active": true
   },
   "usjs18":{
-    "hover": "LOUISIANA",
-    "url": "#", "target": "same_window",
     "upColor": "#f9d600", "overColor": "#161515", "downColor": "#161515",
     "active": true
   },
   "usjs19":{
-    "hover": "<b><u>MAINE</u></b><br><span style='color: #999;'>*Click to open a webpage*</span>",
-    "url": "#", "target": "same_window",
     "upColor": "#f9d600", "overColor": "#161515", "downColor": "#161515",
     "active": true
   },
   "usjs20":{
-    "hover": "MARYLAND",
-    "url": "#", "target": "same_window",
     "upColor": "#f9d600", "overColor": "#161515", "downColor": "#161515",
     "active": true
   },
   "usjs21":{
-    "hover": "MASSACHUSETTS",
-    "url": "#", "target": "same_window",
     "upColor": "#f9d600", "overColor": "#161515", "downColor": "#161515",
     "active": true
   },
   "usjs22":{
-    "hover": "MICHIGAN",
-    "url": "#", "target": "same_window",
     "upColor": "#f9d600", "overColor": "#161515", "downColor": "#161515",
     "active": true
   },
   "usjs23":{
-    "hover": "MINNESOTA",
-    "url": "#", "target": "same_window",
     "upColor": "#f9d600", "overColor": "#161515", "downColor": "#161515",
     "active": true
   },
   "usjs24":{
-    "hover": "MISSISSIPPI",
-    "url": "#", "target": "same_window",
     "upColor": "#f9d600", "overColor": "#161515", "downColor": "#161515",
-    "active": false
+    "active": true
   },
   "usjs25":{
-    "hover": "<b><u>MISSOURI</u></b><br>Write any text and load images<br><img src='example.png'>",
-    "url": "#", "target": "same_window",
     "upColor": "#f9d600", "overColor": "#161515", "downColor": "#161515",
     "active": true
   },
   "usjs26":{
-    "hover": "MONTANA",
-    "url": "#", "target": "same_window",
     "upColor": "#f9d600", "overColor": "#161515", "downColor": "#161515",
     "active": true
   },
   "usjs27":{
-    "hover": "NEBRASKA",
-    "url": "#", "target": "same_window",
     "upColor": "#f9d600", "overColor": "#161515", "downColor": "#161515",
     "active": true
   },
   "usjs28":{
-    "hover": "NEVADA",
-    "url": "#", "target": "same_window",
     "upColor": "#f9d600", "overColor": "#161515", "downColor": "#161515",
     "active": true
   },
   "usjs29":{
-    "hover": "NEW HAMPSHIRE",
-    "url": "#", "target": "same_window",
     "upColor": "#f9d600", "overColor": "#161515", "downColor": "#161515",
     "active": true
   },
   "usjs30":{
-    "hover": "NEW JERSEY",
-    "url": "#", "target": "same_window",
     "upColor": "#f9d600", "overColor": "#161515", "downColor": "#161515",
     "active": true
   },
-  "usjs31":{
-    "hover": "<b><u>NEW MEXICO</u></b><br><span style='color: #999;'>*Click to open a webpage*</span>",
-    "url": "#", "target": "same_window",
     "upColor": "#f9d600", "overColor": "#161515", "downColor": "#161515",
     "active": true
   },
   "usjs32":{
-    "hover": "NEW YORK",
-    "url": "#", "target": "same_window",
     "upColor": "#f9d600", "overColor": "#161515", "downColor": "#161515",
     "active": true
   },
   "usjs33":{
-    "hover": "NORTH CAROLINA",
-    "url": "#", "target": "same_window",
     "upColor": "#f9d600", "overColor": "#161515", "downColor": "#161515",
     "active": true
   },
   "usjs34":{
-    "hover": "NORTH DAKOTA",
-    "url": "#", "target": "same_window",
     "upColor": "#f9d600", "overColor": "#161515", "downColor": "#161515",
     "active": true
   },
   "usjs35":{
-    "hover": "OHIO",
-    "url": "#", "target": "same_window",
     "upColor": "#f9d600", "overColor": "#161515", "downColor": "#161515",
     "active": true
   },
   "usjs36":{
-    "hover": "OKLAHOMA",
-    "url": "#", "target": "same_window",
     "upColor": "#f9d600", "overColor": "#161515", "downColor": "#161515",
     "active": true
   },
   "usjs37":{
-    "hover": "OREGON",
-    "url": "#", "target": "same_window",
     "upColor": "#f9d600", "overColor": "#161515", "downColor": "#161515",
     "active": true
   },
-  "usjs38":{
-    "hover": "<b><u>PENNSYLVANIA</u></b><br>Write any text and load images<br><img src='example.png'>",
-    "url": "#", "target": "same_window",
     "upColor": "#f9d600", "overColor": "#161515", "downColor": "#161515",
     "active": true
   },
   "usjs39":{
-    "hover": "RHODE ISLAND",
-    "url": "#", "target": "same_window",
     "upColor": "#f9d600", "overColor": "#161515", "downColor": "#161515",
     "active": true
   },
   "usjs40":{
-    "hover": "SOUTH CAROLINA",
-    "url": "#", "target": "same_window",
     "upColor": "#f9d600", "overColor": "#161515", "downColor": "#161515",
     "active": true
   },
   "usjs41":{
-    "hover": "SOUTH DAKOTA",
-    "url": "#", "target": "same_window",
     "upColor": "#f9d600", "overColor": "#161515", "downColor": "#161515",
     "active": true
   },
   "usjs42":{
-    "hover": "<b><u>TENNESSEE</u></b><br><span style='color: #999;'>*Click to open a webpage*</span>",
-    "url": "#", "target": "same_window",
     "upColor": "#f9d600", "overColor": "#161515", "downColor": "#161515",
     "active": true
   },
   "usjs43":{
-    "hover": "TEXAS",
-    "url": "#", "target": "same_window",
     "upColor": "#f9d600", "overColor": "#161515", "downColor": "#161515",
     "active": true
   },
   "usjs44":{
-    "hover": "UTAH",
-    "url": "#", "target": "same_window",
     "upColor": "#f9d600", "overColor": "#161515", "downColor": "#161515",
     "active": true
   },
   "usjs45":{
-    "hover": "VERMONT",
-    "url": "#", "target": "same_window",
     "upColor": "#f9d600", "overColor": "#161515", "downColor": "#161515",
     "active": true
   },
   "usjs46":{
-    "hover": "VIRGINIA",
-    "url": "#", "target": "same_window",
     "upColor": "#f9d600", "overColor": "#161515", "downColor": "#161515",
-    "active": false
+    "active": true
   },
   "usjs47":{
-    "hover": "WASHINGTON",
-    "url": "#", "target": "same_window",
     "upColor": "#f9d600", "overColor": "#161515", "downColor": "#161515",
     "active": true
   },
   "usjs48":{
-    "hover": "WEST VIRGINIA",
-    "url": "#", "target": "same_window",
     "upColor": "#f9d600", "overColor": "#161515", "downColor": "#161515",
     "active": true
   },
   "usjs49":{
-    "hover": "WISCONSIN",
-    "url": "#", "target": "same_window",
     "upColor": "#f9d600", "overColor": "#161515", "downColor": "#161515",
     "active": true
   },
   "usjs50":{
-    "hover": "WYOMING",
-    "url": "#", "target": "same_window",
     "upColor": "#f9d600", "overColor": "#161515", "downColor": "#161515",
     "active": true
   },
   "usjs51":{
-    "hover": "WASHINGTON DC",
-    "url": "#", "target": "same_window",
     "upColor": "#f9d600", "overColor": "#161515", "downColor": "#161515",
     "active": true
   },
